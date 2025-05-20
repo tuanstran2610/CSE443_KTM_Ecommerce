244114c2cc0ff2504cfb69ac96cfa575d6c3e9b4
TuansTran <148312811+TuansTran@users.noreply.github.com>
2025-05-21 00:22:26 +0700

    Add project files.

---
+++ CSE443_KTM_Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSE443_KTM_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
