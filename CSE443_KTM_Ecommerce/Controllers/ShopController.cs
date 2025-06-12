0f0f2071b0124023f2cdc100f109a2712b6bef66
Le Vo Khanh Minh <khanhminh.work@gmail.com>
2025-06-12 17:19:08 +0700

    updated with Product Detail Page and Search Page

--- CSE443_KTM_Ecommerce/Controllers/ShopController.cs
+++ CSE443_KTM_Ecommerce/Controllers/ShopController.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSE443_KTM_Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSE443_KTM_Ecommerce.Controllers
 {
     public class ShopController : Controller
     {
+
+        private readonly KTMDbContext _context;
+        public ShopController(KTMDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+          
             return View();
         }
         public IActionResult ShopList()
         {
             return View();
         }
+
+
         public IActionResult Product()
         {
             return View();
